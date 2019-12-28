6e6b3547e67ffb3f9b4f286bd3030fd27a959cd3
Remus <remusiftimie87@gmail.com>
2019-12-28 19:08:51 +0000

    First commit

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab05_Unit_Testing
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+        public double Divide(double value1, double value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
